e48f101d3a0fd061633152072d6690f91275c32e
Hungchen Fu <sean14xx@gmail.com>
2020-12-01 16:38:22 +0100

    feat: Add Quiz

---
+++ components/Quiz.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class Quiz extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Quiz</Text>
+      </View>
+    )
+  }
+}
+
+export default Quiz
