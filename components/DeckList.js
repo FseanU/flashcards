7e01095ed776089f8409d411ba8f98651be84504
Hungchen Fu <sean14xx@gmail.com>
2020-12-01 16:37:28 +0100

    feat: Add DeckList

---
+++ components/DeckList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class DeckList extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>DeckList</Text>
+      </View>
+    )
+  }
+}
+
+export default DeckList
