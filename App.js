dcc71885458829cee0b5a196d3fdded3127110d8
Hungchen Fu <sean14xx@gmail.com>
2020-12-01 16:36:47 +0100

    feat: Add NewQuestion

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import DeckList from './components/DeckList'
+import NewQuestion from './components/NewQuestion';
+import NewDeck from './components/NewDeck';
+import Quiz from './components/Quiz';
+import Deck from './components/Deck';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <DeckList />
+      <NewQuestion />
+      <NewDeck />
+      <Quiz />
+      <Deck />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ components/NewQuestion.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class NewQuestion extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>New Question</Text>
+      </View>
+    )
+  }
+}
+
+export default NewQuestion
