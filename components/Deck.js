fd2a9f69524fac2ba4add3588b8ad7411b19b007
Hungchen Fu <sean14xx@gmail.com>
2020-12-01 16:37:08 +0100

    feat: Add Deck

---
+++ components/Deck.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Deck() {
+  return (
+    <View>
+      <Text>
+        A deck
+      </Text>
+    </View>
+  )
+}
