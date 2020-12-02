1164150265b608eb14a47fade6c41649b4b7d068
Hungchen Fu <sean14xx@gmail.com>
2020-12-02 10:13:45 +0100

    feat: Create store

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View } from 'react-native';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import reducer from './reducers'
 import DeckList from './components/DeckList'
 import NewQuestion from './components/NewQuestion';
 import NewDeck from './components/NewDeck';
@@ -9,12 +12,14 @@ import Deck from './components/Deck';
 
 export default function App() {
   return (
-    <View>
-      <DeckList />
-      <NewQuestion />
-      <NewDeck />
-      <Quiz />
-      <Deck />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View>
+        <DeckList />
+        <NewQuestion />
+        <NewDeck />
+        <Quiz />
+        <Deck />
+      </View>
+    </Provider>
   );
 }
