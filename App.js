0ef4ae87b392eeeac858bcc54a1fbffd649f361b
Hungchen Fu <sean14xx@gmail.com>
2020-12-03 18:21:11 +0100

    feat: Add basic UI to NewQuestion

--- App.js
+++ App.js
@@ -15,6 +15,7 @@ export default function App() {
     <Provider store={createStore(reducer)}>
       <View>     
         <NewDeck />
+        <NewQuestion />
       </View>
     </Provider>
   );
--- components/NewQuestion.js
+++ components/NewQuestion.js
@@ -1,11 +1,60 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button, TextInput } from 'react-native'
 
 class NewQuestion extends React.Component {
+  state = {
+    question: '',
+    answer: ''
+  }
+
+  handleInputChange = (input, name) => {
+    switch (name) {
+      case 'question' :
+        this.setState({question: input})
+        break
+      case 'answer' :
+        this.setState({answer: input})
+        break
+      default :
+        console.log('please add a name to input field')
+    }    
+  }
+
+  onPress = () => {
+    
+    // todo: Update store
+    // todo: Update localstorage
+  
+    this.setState({
+      question: '',
+      answer: '',
+    })
+    
+    // todo: Navigate to Deck view
+    
+  }
+
   render() {
+    const questionValue = this.state.question
+    const answerValue = this.state.answer
+    
     return (
       <View>
         <Text>New Question</Text>
+        <TextInput 
+          onChangeText={input => this.handleInputChange(input, 'question')}
+          value={questionValue}
+          placeholder='Please enter your question'
+        />
+        <TextInput 
+          onChangeText={input => this.handleInputChange(input, 'answer')}
+          value={answerValue}
+          placeholder='Please enter your answer'
+        />
+        <Button 
+          title="Create Question"
+          onPress={this.onPress}
+        />
       </View>
     )
   }
