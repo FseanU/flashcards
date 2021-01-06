0d416d60bdf0f3377b9b35e48badb1811c9a917b
Hungchen Fu <sean14xx@gmail.com>
2021-01-06 14:12:34 +0100

    feat: Set initial data for AsyncStorage

--- components/DeckList.js
+++ components/DeckList.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { View, Text, TouchableOpacity } from 'react-native'
+import { saveExampleDecks, getDecks, setInitialData, saveDeckTitle, addCardToDeck, getDeck, clearAll } from '../utils/api'
 
 class DeckList extends React.Component {
-  onPress = () =>{
+  onPress = () => {
     // todo: Redirect to Deck
+  }
+
+  async componentDidMount() {
+    console.log('decklist mounted')
+  
+    // set initial data for AsyncStorage 
+    getDecks().then(decks => decks ? null : setInitialData()) 
+    getDecks().then(decks => console.log(decks))
   }
 
   render() {
